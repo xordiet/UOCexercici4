a7383da3db1981c65b9fd92789696d3fe044e3dd
Jordi Borràs i Vivó <xordiet@users.noreply.github.com>
2017-12-29 20:28:37 +0100

    exercici 2 mig ok whatsapp (la imatge no agafa extensió). Començant exercici 3 carrega el html a un webview

--- app/src/main/java/edu/uoc/pac1/BookDetailActivity.java
+++ app/src/main/java/edu/uoc/pac1/BookDetailActivity.java
@@ -1,6 +1,9 @@
 package edu.uoc.pac1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +16,9 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * An activity representing a single Book detail screen. This
@@ -38,9 +44,11 @@ public class BookDetailActivity extends AppCompatActivity {
                 myWebView.setVisibility(View.VISIBLE);
                 //WebSettings webSettings = myWebView.getSettings();
                 //webSettings.setJavaScriptEnabled(true);
-                myWebView.setWebViewClient(new WebViewClient());
+                //myWebView.setWebViewClient(new WebViewClient());
+                myWebView.setWebViewClient(new MyWebViewClient());
                 myWebView.loadUrl("file:///android_asset/form.html");
                 findViewById(R.id.fab).setVisibility(View.GONE);
+
             }
         });
 
@@ -65,6 +73,25 @@ public class BookDetailActivity extends AppCompatActivity {
             showDetail(0);
         }
     }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            String dades = new String(Uri.parse(url).getQuery().toString());
+            Uri uri = Uri.parse(url);
+            List<String> args = uri.getQueryParameters("&");
+            if (dades.equals("name=&num=&date=&buy=Envia")){
+                Snackbar.make(view, "has d'emplenar el formulari, gràcies", Snackbar.LENGTH_LONG)
+                        .setAction("mesInfo", null).show();
+            } else {
+                Snackbar.make(view, dades, Snackbar.LENGTH_LONG)
+                        .setAction("mesInfo", null).show();
+
+            }
+            return true;
+        }
+    }
+
 
     private void showDetail(int pos) {
         Bundle arguments = new Bundle();
